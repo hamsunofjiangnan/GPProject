9634e71eefd997d4e739a43af356db384f3b04e8
hamsunofjiangnan <2531978662@qq.com>
2019-03-18 23:39:10 +0800

    工厂模式UML类图

--- SinglePattern/src/main/java/com/itcast/hungry/test.java
+++ SinglePattern/src/main/java/com/itcast/hungry/test.java
@@ -2,7 +2,7 @@ package com.itcast.hungry;
 
 import java.lang.reflect.Constructor;
 
-public class test {
+public class Test {
     public static void main(String[] args) throws Exception {
         //HungrySingleton.getInstance();
         Class<?> clazz = HungrySingleton.class;
